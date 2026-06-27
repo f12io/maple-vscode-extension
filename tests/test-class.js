08cf5e44a07699daab4113753ed6aefc02f93b7a
Onur Adsay <onuradsay@gmail.com>
2026-06-27 14:03:48 +0300

    test: add utility class parsing test cases

--- tests/test-class.js
+++ tests/test-class.js
@@ -33,7 +33,7 @@ export class MyComponent {
       m-${isActive ? '2' : '3'}
       o-50 fw-normal
       ${isActive ? 'fs-50' : 'fs-60'}
-      ${isActive ? `fs-50 m-${isActive ? '2' : '3'}` : `fs-60 m-${isActive ? '2' : '3'}`}
+      ${isActive ? `fs-50 m-${isActive ? '2' : '3'} bgc-red p-2 o-50` : `fs-60 m-${isActive ? '2' : '3'}`}
     `;
 
     return testClass;
